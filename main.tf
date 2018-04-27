31426b81fb2304e0fee35277924567982a1685e4
Oleg Gorodnitchi <oleg.gorodnitchi@sk.ibm.com>
2018-04-27 16:44:45 +0200

    multiple changes

--- main.tf
+++ main.tf
@@ -74,13 +74,13 @@ variable "chmod_command" {
 ###############################################################################
 
 locals {
-  public_key_filename  = "${var.ssh_public_key_path}/${module.label.id}${var.public_key_extension}"
-  private_key_filename = "${var.ssh_public_key_path}/${module.label.id}${var.private_key_extension}"
+  public_key_filename  = "${var.ssh_public_key_path}/${var.name}${var.public_key_extension}"
+  private_key_filename = "${var.ssh_public_key_path}/${var.name}${var.private_key_extension}"
 }
 
 resource "aws_key_pair" "imported" {
   count      = "${var.generate_ssh_key == "false" ? 1 : 0}"
-  key_name   = "${module.label.id}"
+  key_name   = "${var.name}"
   public_key = "${file("${local.public_key_filename}")}"
 }
 
@@ -92,7 +92,7 @@ resource "tls_private_key" "default" {
 resource "aws_key_pair" "generated" {
   count      = "${var.generate_ssh_key == "true" ? 1 : 0}"
   depends_on = ["tls_private_key.default"]
-  key_name   = "${module.label.id}"
+  key_name   = "${var.name}"
   public_key = "${tls_private_key.default.public_key_openssh}"
 }
 
